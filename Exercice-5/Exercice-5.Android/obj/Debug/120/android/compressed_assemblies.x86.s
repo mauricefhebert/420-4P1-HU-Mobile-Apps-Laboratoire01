b2f3bb9e20805c720ca38912adb8a14afd8745ec
mauricefh <filiatreaultmaurice@gmail.com>
2022-05-28 17:58:30 -0400

    Update

--- Exercice-5/Exercice-5.Android/obj/Debug/120/android/compressed_assemblies.x86.s
+++ Exercice-5/Exercice-5.Android/obj/Debug/120/android/compressed_assemblies.x86.s
@@ -1,18 +1,11 @@
-	.file	"obj\Debug\120\android\compressed_assemblies.x86.s"
-
-	.section	.data, "aw", @progbits
-
-	#
-	# Generated from instance of: Xamarin.Android.Tasks.CompressedAssembliesNativeAssemblyGenerator+CompressedAssemblies, Xamarin.Android.Build.Tasks, Version=12.3.0.26, Culture=neutral, PublicKeyToken=84e04ff9cfb79065
-	#
+	.file	"compressed_assemblies.x86.x86.s"
+	.section	.data.compressed_assemblies,"aw",@progbits
 	.type	compressed_assemblies, @object
-	.global	compressed_assemblies
-
-	.section	.data, "aw", @progbits
 	.p2align	2
+	.global	compressed_assemblies
 compressed_assemblies:
-	.long	0x0	# count
-	.long	0	# descriptors
+	/* count */
+	.long	0
+	/* descriptors */
+	.long	0
 	.size	compressed_assemblies, 8
-
-	.ident	"Xamarin.Android remotes/origin/d17-2 @ bbba5a21f3b649a761b22d83959758b4d30df672"
--- Exercice-5/Exercice-5/MainPage.xaml.cs
+++ Exercice-5/Exercice-5/MainPage.xaml.cs
@@ -14,11 +14,14 @@ namespace Exercice_5
         public MainPage()
         {
             InitializeComponent();
+            //Add Event to the button on application launch
             Button_Event_Handler();
         }
-        public string UserInputDisplay = "";
-        double sum = 0;
 
+        //Hold that user calculation interface
+        public string UserInputDisplay = "";
+
+        //Add event to all the button
         public void Button_Event_Handler()
         {
             foreach (var btn in calculator.Children)
@@ -30,54 +33,51 @@ namespace Exercice_5
             }
         }
 
+        //Handle the display of the user calculation interface
         private void Display(object sender, EventArgs e)
         {
+            //If the character is container in the array they will not be displayed in the interface, otherwise they will
             string[] strings = { "(", ")", "C", "=", "⌫" };
             foreach (string s in strings)
             {
                 if (((Button)sender).Text.Contains(s)) return;
             }
 
+            //If the button clicker is not a digit i will append space before and after the character, otherwise i will not
             if (!Char.IsDigit(char.Parse(((Button)sender).Text)))
             {
-                if (sum > 0)
-                {
-                    UserInputDisplay += sum.ToString();
-                }
                 UserInputDisplay += $" {((Button)sender).Text} ";
             }
             else
             {
                 UserInputDisplay += ((Button)sender).Text;
             }
+            //Update the user calculator interface
             UserDisplay.Text = UserInputDisplay;
         }
 
-        private void Button_Clicked_1(object sender, EventArgs e)
+        //Handle the equal button
+        private void Button_Equal(object sender, EventArgs e)
         {
             string[] value = UserInputDisplay.Split(' ');
 
             for (int i = 0; i < value.Length; i++)
             {
-                if (sum > 0)
-                {
-                    UserInputDisplay = "";
-                }
-                if (value[i] == "+") sum = double.Parse(value[i - 1]) + double.Parse(value[i + 1]);
+                
             }
-            UserDisplay.Text = sum.ToString();
         }
 
+        //Clear the user calculator interface
         private void Button_Clear(object sender, EventArgs e)
         {
             UserInputDisplay = "";
             UserDisplay.Text = UserInputDisplay;
         }
 
+        //Backspace one characters from the user calculator interface
         private void Button_Delete(object sender, EventArgs e)
         {
             if (UserInputDisplay.Length <= 0) return;
-
             UserInputDisplay = UserInputDisplay.Remove(UserInputDisplay.Length - 1);
             UserDisplay.Text = UserInputDisplay;
         }
